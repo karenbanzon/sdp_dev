2c8f0bae86e99a5253256dab3b14db431da8e4f6
Karen Banzon <kabanzon@gmail.com>
2013-09-03 05:25:00 +0800

    Added form

--- web/static/form.php
+++ web/static/form.php
@@ -1,168 +1,175 @@
+<?php $thisPage="form" ?>
 <?php require 'includes/header.php' ?>
 
 		<section class="main">
 
-			<nav class="breadcrumb">
-				<a href="">UP Sigma Delta Phi</a> >
-				<a href="" class="active">Membership Requirements</a>
-			</nav>
-
 			<div class="content" id="pinkForm">
-				<h1>Membership Qualifications</h1>
-				<div class="qualifications">
-					<ul>
-						<li>Leader</li>
-						<li>Open-minded</li>
-						<li>Positive-thinker</li>
-						<li>Achiever</li>
-						<li>Team Player</li>
-						<li>Responsible</li>
-						<li>Confident</li>
-					</ul>
-					<ul>
-						<li>At least sophomore standing</li>
-						<li>Enrolled UP student</li>
-						<li>Transferees must have at least 1 year college experience</li>
-					</ul>
+				<div class="bg"></div>
+				<div class="primaryBlock">
+					<h1>Interested?</h1>
+					<h4>Start here.</h4>
+					<a href=""><span class="icon iconpencil" aria-hidden="true"></span></a></span>
+					<div class="video">
+						<iframe src="https://www.facebook.com/video/embed?video_id=10151351924767143" width="800" height="600" frameborder="0"></iframe>
+					</div>
 				</div>
-				<div class="form">
-					<h4>If you are interested, fill out the form below so we can get in touch with you.</h3>
-					<form>						
-						<fieldset>
-							<h6>Basic Information</h6>
-							<div>
-								First Name
-								<input type="text"></input>
-							</div>
-							<div>
-								Middle Name
-								<input type="text"></input>
-							</div>
-							<div>
-								Surname
-								<input type="text"></input>
-							</div>
-							<div>
-								Nickname
-								<input type="text"></input>
-							</div>
-							<div>
-								Current Address
-								<textarea rows="4"></textarea>
-							</div>
-							<div>
-								Permanent Address
-								<textarea rows="4"></textarea>
-							</div>
-							<div>
-								Birthdate<br/>
-								<select class="small">
-								    <option value="" disabled selected>Select month</option>
-								    <option>January</option>
-								    <option>February</option>
-								    <option>March</option>
-								    <option>April</option>
-								    <option>May</option>
-								    <option>June</option>
-								    <option>July</option>
-								    <option>August</option>
-								    <option>September</option>
-								    <option>October</option>
-								    <option>November</option>
-								    <option>December</option>
-								</select>
-								<select class="small">
-								    <option value="" disabled selected>Select date</option>
-								    <option>01</option>
-								    <option>02</option>
-								    <option>03</option>
-								    <option>04</option>
-								    <option>05</option>
-								    <option>06</option>
-								    <option>07</option>
-								    <option>08</option>
-								    <option>09</option>
-								    <option>10</option>
-								    <option>11</option>
-								    <option>12</option>
-								</select>
-								<select class="small">
-								    <option value="" disabled selected>Select year</option>
-								    <option>1990</option>
-								    <option>1991</option>
-								    <option>1992</option>
-								    <option>1993</option>
-								    <option>1994</option>
-								    <option>1995</option>
-								    <option>1996</option>
-								    <option>1997</option>
-								    <option>1998</option>
-								    <option>1999</option>
-								    <option>2000</option>
-								    <option>2001</option>
-								</select>
-							</div>
-							<div>
-								Age<br/>
-								<select class="small">
-								    <option value="" disabled selected>Select age</option>
-								    <option>16</option>
-								    <option>17</option>
-								    <option>18</option>
-								    <option>19</option>
-								    <option>20</option>
-								    <option>21</option>
-								    <option>22</option>
-								    <option>23</option>
-								</select>
-							</div>
-							<div>
-								Mobile Number
-								<input type="text"></input>
-							</div>
-							<div>
-								Email Address
-								<input type="text"></input>
-							</div>
-							<div>
-								Year &amp; Course
-								<input type="text"></input>
-							</div>
-							<div>
-								College
-								<input type="text"></input>
-							</div>
-							<div>
-								Student Number
-								<input type="text"></input>
-							</div>
-							<div>
-								General Weighted Average<br />
-								<input type="text" class="small"></input>
-							</div>
-							<div>
-								Academic Achievements
-								<textarea rows="4"></textarea>
-							</div>
-							<div>
-								Extracurricular Activities
-								<textarea rows="4"></textarea>
-							</div>
-							<div>
-								Sigma Deltan Relatives (if any)
-								<input type="text"></input>
-							</div>
-							<div>
-								Upsilonian Relatives (if any)
-								<input type="text"></input>
-							</div>
-							<div>
-								How did you find out about UP Sigma Delta Phi?
-								<input type="text"></input>
-							</div>
-						</fieldset>
-					</form>
-				</div>
+				<div class="block">
+					<h3>Membership Qualifications</h3>
+					<div class="qualifications">
+						<ul>
+							<li>Leader</li>
+							<li>Open-minded</li>
+							<li>Positive-thinker</li>
+							<li>Achiever</li>
+							<li>Team Player</li>
+							<li>Responsible</li>
+							<li>Confident</li>
+						</ul>
+						<ul>
+							<li>At least sophomore standing</li>
+							<li>Enrolled UP student</li>
+							<li>Transferees must have at least 1 year college experience</li>
+						</ul>
+					</div>
+					<div class="form">
+						<h4>Fill out the form below so we can get in touch with you.</h3>
+						<form>						
+							<fieldset>
+								<h6>Basic Information</h6>
+								<div>
+									First Name
+									<input type="text"></input>
+								</div>
+								<div>
+									Middle Name
+									<input type="text"></input>
+								</div>
+								<div>
+									Surname
+									<input type="text"></input>
+								</div>
+								<div>
+									Nickname
+									<input type="text"></input>
+								</div>
+								<div>
+									Current Address
+									<textarea rows="4"></textarea>
+								</div>
+								<div>
+									Permanent Address
+									<textarea rows="4"></textarea>
+								</div>
+								<div>
+									Birthdate<br/>
+									<select class="small">
+									    <option value="" disabled selected>Select month</option>
+									    <option>January</option>
+									    <option>February</option>
+									    <option>March</option>
+									    <option>April</option>
+									    <option>May</option>
+									    <option>June</option>
+									    <option>July</option>
+									    <option>August</option>
+									    <option>September</option>
+									    <option>October</option>
+									    <option>November</option>
+									    <option>December</option>
+									</select>
+									<select class="small">
+									    <option value="" disabled selected>Select date</option>
+									    <option>01</option>
+									    <option>02</option>
+									    <option>03</option>
+									    <option>04</option>
+									    <option>05</option>
+									    <option>06</option>
+									    <option>07</option>
+									    <option>08</option>
+									    <option>09</option>
+									    <option>10</option>
+									    <option>11</option>
+									    <option>12</option>
+									</select>
+									<select class="small">
+									    <option value="" disabled selected>Select year</option>
+									    <option>1990</option>
+									    <option>1991</option>
+									    <option>1992</option>
+									    <option>1993</option>
+									    <option>1994</option>
+									    <option>1995</option>
+									    <option>1996</option>
+									    <option>1997</option>
+									    <option>1998</option>
+									    <option>1999</option>
+									    <option>2000</option>
+									    <option>2001</option>
+									</select>
+								</div>
+								<div>
+									Age<br/>
+									<select class="small">
+									    <option value="" disabled selected>Select age</option>
+									    <option>16</option>
+									    <option>17</option>
+									    <option>18</option>
+									    <option>19</option>
+									    <option>20</option>
+									    <option>21</option>
+									    <option>22</option>
+									    <option>23</option>
+									</select>
+								</div>
+								<div>
+									Mobile Number
+									<input type="text"></input>
+								</div>
+								<div>
+									Email Address
+									<input type="text"></input>
+								</div>
+								<div>
+									Year &amp; Course
+									<input type="text"></input>
+								</div>
+								<div>
+									College
+									<input type="text"></input>
+								</div>
+								<div>
+									Student Number
+									<input type="text"></input>
+								</div>
+								<div>
+									General Weighted Average<br />
+									<input type="text" class="small"></input>
+								</div>
+								<div>
+									Academic Achievements
+									<textarea rows="4"></textarea>
+								</div>
+								<div>
+									Extracurricular Activities
+									<textarea rows="4"></textarea>
+								</div>
+								<div>
+									Sigma Deltan Relatives (if any)
+									<input type="text"></input>
+								</div>
+								<div>
+									Upsilonian Relatives (if any)
+									<input type="text"></input>
+								</div>
+								<div>
+									How did you find out about UP Sigma Delta Phi?
+									<input type="text"></input>
+								</div>
+							</fieldset>
+						</form>
+					</div>
+				</block>
 			</div>
 		</section>
 
