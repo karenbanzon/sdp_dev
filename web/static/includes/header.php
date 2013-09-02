90455467c37f255251caab1e39f10427b537ad79
Karen Banzon <kabanzon@gmail.com>
2013-09-03 04:47:53 +0800

    Added PBMK page

--- web/static/includes/header.php
+++ web/static/includes/header.php
@@ -12,22 +12,22 @@
 		<header class="siteHead">
 			<div id="brand">
 				<div>
-					<span class="icon iconseal" aria-hidden="true"></span>
-					<div class="name">
-						<p class="greekName">UP SIGMA DELTA PHI</p>
-						<p class="englishName">Society of Dramatics and Fine Arts
-					</div>
-					<div class="social">
+					<img class="seal" src="/images/essential/seal.png">
+					<!-- <div class="social">
 						<a href="http://facebook.com/80kph" rel="nofollow"><span class="icon iconfacebook" aria-hidden="true"></span></a>
 						<a href="http://twitter.com/upsigmadeltaphi" rel="nofollow"><span class="icon icontwitter" aria-hidden="true"></span></a>
 						<a href=""><span class="icon iconpinterest" aria-hidden="true"></span></a>
-					</div>		
+					</div> -->
+					<nav>
+						<a href="about-us.php" <?php if ($thisPage=="about") 
+	echo " class=\"active\""; ?>>About Us</a>
+						<a href="project-bata-mag-aral-ka.php" <?php if ($thisPage=="pbmk") 
+	echo " class=\"active\""; ?>>Project Bata, Mag-aral Ka</a>
+						<a href="up-powercard.php" <?php if ($thisPage=="uppc") 
+	echo " class=\"active\""; ?>>UP Powercard</a>
+						<a href="contact-us.php" <?php if ($thisPage=="contact") 
+	echo " class=\"active\""; ?>>Contact Us</a>
+					</nav>	
 				</div>
 			</div>
-			<nav>
-				<a href="">About Us</a>
-				<a href="">Project Bata, Mag-aral Ka</a>
-				<a href="">UP Powercard</a>
-				<a href="">Contact Us</a>
-			</nav>
 		</header>
--- web/static/index.php
+++ web/static/index.php
@@ -1,13 +1,43 @@
+<?php $thisPage="home" ?>
 <?php require 'includes/header.php' ?>
 
 		<section class="main">
 			<div class="content" id="homepage">
+				<div class="bg"></div>
 				<div class="primaryBlock">
+					<h1>We are what we say.</h1>
+					<h4>Hear it from us</h4>
+					<a href=""><span class="icon iconplay" aria-hidden="true"></a></span>
 					<div class="video">
 						<iframe src="https://www.facebook.com/video/embed?video_id=10151351924767143" width="800" height="600" frameborder="0"></iframe>
 					</div>
-					<a href="form.php" class="button">Membership Qualifications <span class="icon iconarrowright"></span></a>
 				</div>
+				<div class="block">
+					<h3>What we stand for</h3>
+					<div class="pillars">
+						<div>
+							<img src="http://placehold.it/150x150">
+							<h6>Leadership</h6>
+							<div>
+								Lorem ipsum dolor sit amet, adipiscing consecteteur sit.
+							</div>
+						</div>
+						<div>
+							<img src="http://placehold.it/150x150">
+							<h6>Excellence</h6>
+							<div>
+								Lorem ipsum dolor sit amet, adipiscing consecteteur sit.
+							</div>
+						</div>
+						<div>
+							<img src="http://placehold.it/150x150">
+							<h6>Sisterhood</h6>
+							<div>
+								Lorem ipsum dolor sit amet, adipiscing consecteteur sit.
+							</div>
+						</div>
+					</div>
+					<a href="form.php" class="button">Membership Qualifications <span class="icon iconarrowright"></span></a>
 			</div>
 		</section>
 
